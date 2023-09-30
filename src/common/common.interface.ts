73e9cd908e44b8228f2b3289daffec027b725742
Lý Tại Trường <lytaitruong060197@gmail.com>
2023-10-01 01:46:42 +0700

    feat(crud): defined crud base structure to apply using and add dependabot to check vulnerable

--- src/common/common.interface.ts
+++ src/common/common.interface.ts
@@ -1,4 +1,5 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { PaginatedResult } from 'prisma-pagination'
 import { Env } from './common.enum'
 
 export interface IConfigApp {
@@ -23,3 +24,27 @@ export interface IConfig {
 
 export type IReq = FastifyRequest
 export type IRes = FastifyReply
+
+// CRUD Interface
+export type Pag<T> = PaginatedResult<T | Partial<T>>
+export type Res<T> = T | Partial<T>
+
+export interface IBaseService<T, Create, Update, Query, Param, Info = void> {
+  getAll(info: Info, params: Omit<Param, 'id'>, query: Query, ...others: any[]): Promise<Pag<T>>
+  create(info: Info, params: Omit<Param, 'id'>, data: Create, ...others: any[]): Promise<Res<T>>
+  getOne(info: Info, params: Param, ...others: any[]): Promise<Res<T> | null>
+  remove(info: Info, params: Param, ...others: any[]): Promise<Res<T> | null>
+  update(info: Info, params: Param, data: Update, ...others: any[]): Promise<Res<T>>
+}
+
+export interface IBaseController<T, Create, Update, Query, Param> {
+  getAll(req: IReq, params: Omit<Param, 'id'>, query: Query, ...others: any[]): Promise<Pag<T>>
+  create(req: IReq, params: Omit<Param, 'id'>, data: Create, ...others: any[]): Promise<Res<T>>
+  getOne(req: IReq, params: Param, ...others: any[]): Promise<Res<T> | null>
+  remove(req: IReq, params: Param, ...others: any[]): Promise<Res<T> | null>
+  update(req: IReq, params: Param, data: Update, ...others: any[]): Promise<Res<T>>
+}
+
+export class BaseController<T, Create, Update, Query, Param> {
+  constructor(protected service: IBaseService<T, Create, Update, Query, Param>) {}
+}
